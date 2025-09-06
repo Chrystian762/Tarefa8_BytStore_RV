cb0d7e1c13e8f0271930b0e423d9e81478cd76b1
Chrystian762 <chrystiangabriel762@gmail.com>
2025-09-05 22:36:35 -0300

    Tela Finalizada

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Form from '../componente/Form'
+
+function App() {
+
+  return (
+    <div className="App">
+        <h1 className="titulo">Tela de Login</h1>
+        <Form/>
+    </div>
+  )
+}
+
+export default App
