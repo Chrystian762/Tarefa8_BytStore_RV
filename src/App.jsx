db905b906207bbcb75ea77af3319dadf895de60e
Chrystian762 <chrystiangabriel762@gmail.com>
2025-11-14 20:32:33 -0300

    meno atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import { useState } from 'react';
-import SeuNome from '../componente/SeuNome';
-import Saudacao from '../componente/Saudacao';
 import './App.css'
 
 function App() {
 
-  const [nome, setNome] = useState("");
   
   return (
     <div className="App">
-        <h1 className="titulo">State Lift</h1>
-        <SeuNome setNome={ setNome }/>
-        <Saudacao nome={nome}/>
-        {nome &&(
-          <p>O Nome digitado foi: {nome}</p>
-        )}
+        
       
     </div>
   )
